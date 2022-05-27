3380f9e14d0b50d883fe18d76496ac8422508377
bll <757633801@qq.com>
2022-05-27 20:05:21 +0800

    $doPrepareNum

--- application/command.php
+++ application/command.php
@@ -10,10 +10,11 @@
 // +----------------------------------------------------------------------
 
 return [
+    'app\shell\Prepareorder',  //预拉单
     'app\shell\Timecheckorder',  //查单订单表等待付款订单
-    'app\shell\Timenotifyhx',  //查本地未回调且已支付支付核销单，回调核销
+//    'app\shell\Timenotifyhx',  //查本地未回调且已支付支付核销单，回调核销
     'app\shell\Timenotifyorder',  //查本地未回调且已支付支付订单单，回调商户
-    'app\shell\Timeoutorder',  //超时订单修改订单状态
-    'app\shell\Timerestarthxorder',  //解冻核销单，以重新使用
-    'app\shell\Notifynopayhx',  //定时回调核销 支付失败
+//    'app\shell\Timeoutorder',  //超时订单修改订单状态
+//    'app\shell\Timerestarthxorder',  //解冻核销单，以重新使用
+//    'app\shell\Notifynopayhx',  //定时回调核销 支付失败
 ];
