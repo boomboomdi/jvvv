960c5f25298fec447778ca11e709a2aee3ecdb19
bll <757633801@qq.com>
2022-06-13 23:33:12 +0800

    getOrderInfo

--- application/common/model/SystemConfigModel.php
+++ application/common/model/SystemConfigModel.php
@@ -76,7 +76,7 @@ class SystemConfigModel extends Model
 //            if (isset($config['configContent']) && !empty($config['configContent'])) {
 //                return (int)$config['configContent'];
 //            }
-            return 300;
+            return 43200;
         } catch (\Exception $exception) {
             return 300;
         } catch (\Error $error) {
