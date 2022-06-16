848baa34f872cd16d82845ed7108e44c5584f61f
bll <757633801@qq.com>
2022-06-16 12:58:19 +0800

    280

--- application/common/model/SystemConfigModel.php
+++ application/common/model/SystemConfigModel.php
@@ -76,7 +76,7 @@ class SystemConfigModel extends Model
 //            if (isset($config['configContent']) && !empty($config['configContent'])) {
 //                return (int)$config['configContent'];
 //            }
-            return 43200;
+            return 300;
         } catch (\Exception $exception) {
             return 300;
         } catch (\Error $error) {
