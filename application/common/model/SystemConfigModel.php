59fd2a982f58a12159e17a680199b5954c614fbe
bll <757633801@qq.com>
2022-07-21 23:29:56 +0800

    apk

--- application/common/model/SystemConfigModel.php
+++ application/common/model/SystemConfigModel.php
@@ -53,7 +53,7 @@ class SystemConfigModel extends Model
 //            if (isset($config['configContent']) && !empty($config['configContent'])) {
 //                return (int)$config['configContent'];
 //            }
-            return 290;
+            return 1600;
         } catch (\Exception $exception) {
             return 1800;
         } catch (\Error $error) {
